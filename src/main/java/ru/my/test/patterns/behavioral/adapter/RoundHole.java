93b7be12df989d527e75d7282f5746cf8b3d1d46
Vveryasov <vveryasov@mmtr.ru>
2019-02-22 12:25:31 +0300

    Паттерны

---
+++ src/main/java/ru/my/test/patterns/behavioral/adapter/RoundHole.java
@@ -0,0 +1,18 @@
+package ru.my.test.patterns.behavioral.adapter;
+
+public class RoundHole {
+
+    private double radius;
+
+    public RoundHole (double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public boolean fits(RoundPeg peg){
+        return peg.getRadius() <= this.getRadius();
+    }
+}
